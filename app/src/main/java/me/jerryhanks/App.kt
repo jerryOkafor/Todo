c6053e9c17b4a1632fbc46391e6d8a0912fc2ef0
Jerry Hanks <jerryhanksokafor@gmail.com>
2020-07-17 00:39:14 +0100

    add dynamic, instant dynamic and library modules.

---
+++ app/src/main/java/me/jerryhanks/App.kt
@@ -0,0 +1,15 @@
+package me.jerryhanks
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+/**
+ * @author jerry on 17/07/2020
+ * for Todo
+ **/
+
+@HiltAndroidApp
+class App : Application() {
+
+}
